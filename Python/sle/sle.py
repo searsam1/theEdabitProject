ed407e95c227eb0902cb0091864537d6bd6b7af9
Alec <amsears60441@gmail.com>
2022-05-16 03:13:16 -0600

    push extreme challenge

--- Python/sle/sle.py
+++ Python/sle/sle.py
@@ -1,3 +1,32 @@
 
 def sle(m):
-	pass
+	try:
+		arr1,arr2 = m[0],m[1]
+		a,b,c = arr1[0],arr1[1],arr1[2]
+		d,e,f = arr2[0],arr2[1],arr2[2]
+		
+		middle = arr1.pop(1) * -1
+		arr1.append(middle)
+
+		div = arr1.pop(0)
+		arr1 = [i for i in arr1]
+		
+		arr2[1] *= a
+		arr2[2] *= a
+
+		y_s = d * arr1[1] + arr2[1]
+		c = arr2[-1] - d * arr1[0]
+		y = y_s / c
+		
+		x = None
+		if e < 0:
+			x = (f + e) / d
+		else:
+			x = (e - f) / d
+		return (x,y)
+	except Exception:
+		return 0
+sle([[2, 3, 13], [5, -1, 7]])
+
+
+
--- Python/sle/tests.py
+++ Python/sle/tests.py
@@ -5,10 +5,9 @@ class Test(unittest.TestCase):
 	
 	checks = [] 
 	def assert_equals(a,b,message=None,checks=checks):
-		if a != b : print(message) if message else print(a == b)
-		return a == b
+		print(a==b)
 
-t1 = Test.assert_equals(sle([[1, 2, -1], [2, -3, 5]]), (1, -1))
+Test.assert_equals(sle([[1, 2, -1], [2, -3, 5]]), (1, -1))
 Test.assert_equals(sle([[3, -4, 1], [2, 3, 12]]), (3, 2))
 Test.assert_equals(sle([[1, -4, 12], [3, -12, 36]]), False)
 Test.assert_equals(sle([[3, 2, -4], [2, 5, 1]]), (-2, 1))
