658c76137a450edd153c3c7b31c3fe8f36d177bb
Alec <amsears60441@gmail.com>
2022-05-20 23:05:46 -0600

    Add -->max_occur<--

--- Python/is_set/is_set.py
+++ Python/is_set/is_set.py
@@ -1,1 +1,9 @@
-def is_set(cards):pass
+def is_set(cards):
+	colors = len({d.get("color") for d in cards}) in [1,3]
+	nums = len({d.get("number") for d in cards}) in [1,3]
+	shades = len({d.get("shade") for d in cards}) in [1,3]
+	shapes = len({d.get("shape") for d in cards}) in [1,3]
+
+	return colors and nums and shades and shapes
+
+
---
+++ Python/max_occur/max_occur.py
@@ -0,0 +1,2 @@
+def max_occur(text):
+	pass
