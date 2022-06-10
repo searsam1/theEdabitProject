215631a7cb42e657e55571bfb662cbcccb72ebbe
Alec <amsears60441@gmail.com>
2022-06-10 01:09:22 -0600

    Add -->find_all_digits<--

--- Python/change_string/change_string.py
+++ Python/change_string/change_string.py
@@ -1,2 +1,17 @@
-def change_string(word):
-	pass
+
+# from string import ascii_uppercase
+import string
+str_asc_up = string.ascii_uppercase
+d = {j : i for i,j in enumerate(str_asc_up)}
+d_r = {i : j for i,j in enumerate(str_asc_up)}
+
+def change_string(s):
+	
+	key = d
+	res = [i.upper() if 
+			i.islower() 
+				else 
+				d_r[d[i] + 1 if d[i] != 25 else 0]
+				for i in s][::-1]
+	return "".join(res)
+	
---
+++ Python/find_all_digits/find_all_digits.py
@@ -0,0 +1,2 @@
+def find_all_digits(nums):
+	pass
