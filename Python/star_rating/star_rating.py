a8d83b6b8ab8f6c4c39efbf96d8bcbd6d8030962
Alec <amsears60441@gmail.com>
2022-06-23 11:29:59 -0600

    fuck

--- Python/star_rating/star_rating.py
+++ Python/star_rating/star_rating.py
@@ -1,2 +1,9 @@
 def star_rating(lst):
-	pass
+	total_stars = sum((idx+1) * i for idx, i in enumerate(lst))
+	avg_rating = total_stars  / sum(lst)
+	string = "[{0:.2f}] {1}".format(avg_rating, "*" * round(avg_rating))
+	return string
+
+	
+	
+
