7ece1f0b64e9655b05cfa7953329a116bd5b47fd
Alec <amsears60441@gmail.com>
2022-06-24 15:08:34 -0600

    Create Challenge : ARr5tA458o2tC9FTN on 2022-06-24 15:08:34.065904

--- ARr5tA458o2tC9FTN/code.js
+++ ARr5tA458o2tC9FTN/code.js
@@ -2,6 +2,6 @@ function hello() {
 	
 }
 
- 1function hello() {2  3}
+function hello() {2  3}
 
- Test.assertEquals(hello(), "hello edabit.com", "Did you *return* the result?")
+Test.assertEquals(hello(), "hello edabit.com", "Did you *return* the result?")
