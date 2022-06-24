bf1cee3584ee67567fcd6980e9945d75515a51a1
Alec <amsears60441@gmail.com>
2022-06-24 15:45:00 -0600

    Create Challenge : W3yNHjZdzie2fo2Er on 2022-06-24 15:45:00.943588

--- W3yNHjZdzie2fo2Er/code.js
+++ W3yNHjZdzie2fo2Er/code.js
@@ -1,3 +1,13 @@
 function license(me, agents, others) {
 	
 }
+
+TestsConsoleTest.assertEquals(license("Zebediah", 1, "Bob Jim Becky Pat"), 100)
+
+Test.assertEquals(license("Eric", 2, "Adam Caroline Rebecca Frank"), 40)
+
+Test.assertEquals(license("Aaron", 3, "Jane Max Olivia Sam"), 20)
+
+Test.assertEquals(license("Zebediah", 4, "Bob Jim Becky Pat"), 40)
+
+Test.assertEquals(license("Zebediah", 5, "Bob Jim Becky Pat"), 20)
