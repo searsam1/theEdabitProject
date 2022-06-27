c7af4341c6cf296db042cd20f44fc33cdb116727
Alec <amsears60441@gmail.com>
2022-06-27 11:54:08 -0600

    Create Challenge : Ev4Mq8HqbRn5oA8Yb on 2022-06-27 11:54:08.066950

---
+++ Ev4Mq8HqbRn5oA8Yb/readme.md
@@ -0,0 +1,17 @@
+# Multiply Every Array Item by Two
+<br><br>
+## Matt
+<br><br>
+### """Create a function that takes an array with numbers and return an array with the elements multiplied by two.
+N/A"""
+<br><br>
+[Ev4Mq8HqbRn5oA8Yb](https://edabit.com/challenge/Ev4Mq8HqbRn5oA8Yb)
+<br><br>
+```get_multiplied_arr([2, 5, 3]) ➞ [4, 10, 6]
+
+get_multiplied_arr([1, 86, -5]) ➞ [2, 172, -10]
+
+get_multiplied_arr([5, 382, 0]) ➞ [10, 764, 0]
+```
+
+<br><br>
--- ajxFaZCoQiCwvdrak/code.rb
+++ ajxFaZCoQiCwvdrak/code.rb
@@ -1,23 +1,5 @@
 def n_sided_shape(n)
-	
+	return Array.[]("fuck","circle","semi-circle","triangle","square",
+					"pentagon", "hexagon", "heptagon", "octagon",
+	  				"nonagon", "decagon")[n]
 end
-
-TestsConsoleTest.assert_equals(n_sided_shape(1), "circle")
-
-Test.assert_equals(n_sided_shape(2), "semi-circle")
-
-Test.assert_equals(n_sided_shape(3), "triangle")
-
-Test.assert_equals(n_sided_shape(4), "square")
-
-Test.assert_equals(n_sided_shape(5), "pentagon")
-
-Test.assert_equals(n_sided_shape(6), "hexagon")
-
-Test.assert_equals(n_sided_shape(7), "heptagon")
-
-Test.assert_equals(n_sided_shape(8), "octagon")
-
-Test.assert_equals(n_sided_shape(9), "nonagon")
-
-Test.assert_equals(n_sided_shape(10), "decagon")
