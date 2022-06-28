9cdffcfa3a8905752089e0440b304c7348e6923c
Alec <amsears60441@gmail.com>
2022-06-27 18:31:16 -0600

    Create Challenge : CzYisnM5k7ndRPbey on 2022-06-27 18:31:16.394871

--- Ev4Mq8HqbRn5oA8Yb/code.rb
+++ Ev4Mq8HqbRn5oA8Yb/code.rb
@@ -1,13 +1,6 @@
 def get_multiplied_arr(arr)
-	
+	for i in (0...arr.length())
+	 	arr[i] = arr[i] * 2
+	end
+  	return arr
 end
-
-TestsConsoleTest.assert_equals(get_multiplied_arr([2, 5, 3]), [4, 10, 6])
-
-Test.assert_equals(get_multiplied_arr([1, 86, -5]), [2, 172, -10])
-
-Test.assert_equals(get_multiplied_arr([5, 382, 0]), [10, 764, 0])
-
-
-
-# Made by @felixjoykind
---
+++ javaScript/code.js
@@ -0,0 +1,21 @@
+function height(side) {
+	
+}
+
+TestsConsoleTest.assertEquals(height(2), "17.3 mm")
+
+Test.assertEquals(height(5), "43.3 mm")
+
+Test.assertEquals(height(6.2), "53.7 mm")
+
+Test.assertEquals(height(8.7), "75.3 mm")
+
+Test.assertEquals(height(10), "86.6 mm")
+
+Test.assertEquals(height(13), "112.6 mm")
+
+Test.assertEquals(height(18.5), "160.2 mm")
+
+
+
+// Author: Jessie_02
