cba2a65f43732f7d3dd151a1ab94bc8c82256205
Alec <amsears60441@gmail.com>
2022-07-04 12:00:26 -0600

    Create Challenge : MNJvhq2fXLFbhuti8 on 2022-07-04 12:00:26.723397

--- Python/fB4T399jry7rPofvT/code.py
+++ Python/fB4T399jry7rPofvT/code.py
@@ -1,34 +1,19 @@
 def gpa_calculator(student):
-	
+    class Student():
 
-TestsConsolefirst = {"name": "Utsab Karkee", "courses": [{"name": "cal1", "credit_hours": 5, "grade": "A"}, {"name": "phy1", "credit_hours": 3, "grade": "A"}, {"name": "eng1", "credit_hours": 3, "grade": "B"}, {
-
-    "name": "pysch1", "credit_hours": 3, "grade": "A"}, {"name": "music1", "credit_hours": 3, "grade": "A"}, {"name": "chem1", "credit_hours": 3, "grade": "A"}], "semester": "Fall 2013"}
-
-second = {"name": "Ansha Mandal", "courses": [{"name": "cal1", "credit_hours": 5, "grade": "A"}, {
-
-    "name": "kin1", "credit_hours": 3, "grade": "A"}], "semester": "Spring 2018"}
-
-third = {"name": "Lila Jha", "courses": [{"name": "cal1", "credit_hours": 5, "grade": "B"}, {"name": "phy1", "credit_hours": 3, "grade": "B"}, {"name": "eng1", "credit_hours": 3, "grade": "B"}, {
-
-    "name": "pysch1", "credit_hours": 3, "grade": "B"}, {"name": "music1", "credit_hours": 3, "grade": "C"}, {"name": "chem1", "credit_hours": 3, "grade": "A"}], "semester": "Summer 2018"}
-
-fourth = {"name": "Rama Basnet", "courses": [{"name": "cal3", "credit_hours": 3, "grade": "B"}, {"name": "dan1", "credit_hours": 3, "grade": "C"}, {"name": "eng3", "credit_hours": 3, "grade": "A"}, {
-
-    "name": "pysch1", "credit_hours": 3, "grade": "A"}, {"name": "music1", "credit_hours": 3, "grade": "A"}], "semester": "Spring 2017"}
-
-fifth = {"name": "Rima Rawal", "courses": [{"name": "cal1", "credit_hours": 5, "grade": "A"}, {
-
-    "name": "kin1", "credit_hours": 3, "grade": "F"}], "semester": "Spring 2018"}
-
-
-
-Test.assert_equals(gpa_calculator(first),"Utsab Karkee GPA for Fall 2013 is 3.85")
-
-Test.assert_equals(gpa_calculator(second),"Ansha Mandal GPA for Spring 2018 is 4.00")
-
-Test.assert_equals(gpa_calculator(third),"Lila Jha GPA for Summer 2018 is 3.00")
-
-Test.assert_equals(gpa_calculator(fourth),"Rama Basnet GPA for Spring 2017 is 3.40")
-
-Test.assert_equals(gpa_calculator(fifth),"Rima Rawal GPA for Spring 2018 is 2.50")
+        def __init__(self,info):
+            self.student_name = info["name"]
+            self.semester = info["semester"]
+            self.courses = info["courses"]
+            self.credit_hours = [d["credit_hours"] for d in self.courses]
+            self.grades = [{"A" : 4, "B" : 3, 
+                            "C" : 2, "D" : 1, "F" : 0}[d["grade"]]
+                            for d in self.courses
+                            ]
+            self.gpa = sum(map(lambda x : x[0] * x[1], 
+            zip(self.grades, self.credit_hours))) / sum(self.credit_hours)
+    stud = Student(student)
+    return "{0} GPA for {1} is {2}".format(stud.student_name, 
+	stud.semester, 
+	str(round(stud.gpa,2)).ljust(4,"0")
+	)
