fdcaa18f32db711597fe6194958344de196850a3
Alec <amsears60441@gmail.com>
2022-07-13 18:50:34 -0600

    Create Challenge : Grwj3q6oBfeXxhLq5 on 2022-07-13 18:50:34.234249

---
+++ Python/Grwj3q6oBfeXxhLq5/code.py
@@ -0,0 +1,20 @@
+def sexy_triplets(low, high):
+	
+
+TestsConsoleTest.assert_equals(sexy_triplets(1, 19), [[7, 13, 19]], "Example #1")
+
+Test.assert_equals(sexy_triplets(1, 17), [], "Example #2")
+
+Test.assert_equals(sexy_triplets(64, 88), [[67, 73, 79]], "Example #3")
+
+Test.assert_equals(sexy_triplets(11, 59), [[17, 23, 29], [31, 37, 43], [47, 53, 59]])
+
+Test.assert_equals(sexy_triplets(17, 29), [[17, 23, 29]])
+
+Test.assert_equals(sexy_triplets(109, 275), [[151, 157, 163], [167, 173, 179], [227, 233, 239], [257, 263, 269]])
+
+Test.assert_equals(sexy_triplets(1000, 1080), [])
+
+Test.assert_equals(sexy_triplets(5842, 6333), [[6067, 6073, 6079], [6257, 6263, 6269], [6317, 6323, 6329]])
+
+Test.assert_equals(sexy_triplets(45000, 45777), [[45427, 45433, 45439], [45491, 45497, 45503], [45751, 45757, 45763]])
--- Python/yi2RNQshfhnJvDMSW/code.py
+++ Python/yi2RNQshfhnJvDMSW/code.py
@@ -1,32 +1,26 @@
+def left(lst):
+    res = [] 
+
+    total = 0 
+    for i in range(len(lst)):
+        total += lst[i]
+        res.append( [total] + lst[i+1:] )
+
+    res = res[1:]
+    return [lst] + res
+
+def right(lst):
+    lst = lst[::-1]
+    res = [] 
+
+    total = 0 
+    for i in range(len(lst)):
+        total += lst[i]
+        res.append( ([total] + lst[i+1:])[::-1] )
+
+    res = res[1:]
+    return [lst[::-1]] + res
+
 def squish(lst, d):
-	
 
-TestsConsoleTest.assert_equals(squish([1, 2, 3, 4, 5], "left"), [[1, 2, 3, 4, 5], [3, 3, 4, 5], [6, 4, 5], [10, 5], [15]])
-
-Test.assert_equals(squish([1, 0, 2, -3], "left"), [[1, 0, 2, -3], [1, 2, -3], [3, -3], [0]])
-
-Test.assert_equals(squish([4, 9, -3, 2, 5], "left"), [[4, 9, -3, 2, 5], [13, -3, 2, 5], [10, 2, 5], [12, 5], [17]])
-
-Test.assert_equals(squish([8, -7, 6, 1, 0, 2], "left"), [[8, -7, 6, 1, 0, 2], [1, 6, 1, 0, 2], [7, 1, 0, 2], [8, 0, 2], [8, 2], [10]])
-
-Test.assert_equals(squish([8, 7], "left"), [[8, 7], [15]])
-
-Test.assert_equals(squish([8], "left"), [[8]])
-
-Test.assert_equals(squish([], "left"), [])
-
-
-
-Test.assert_equals(squish([1, 2, 3, 4, 5], "right"), [[1, 2, 3, 4, 5], [1, 2, 3, 9], [1, 2, 12], [1, 14], [15]])
-
-Test.assert_equals(squish([1, 0, 2, -3], "right"), [[1, 0, 2, -3], [1, 0, -1], [1, -1], [0]])
-
-Test.assert_equals(squish([4, 9, -3, 2, 5], "right"), [[4, 9, -3, 2, 5], [4, 9, -3, 7], [4, 9, 4], [4, 13], [17]])
-
-Test.assert_equals(squish([8, -7, 6, 1, 0, 2], "right"), [[8, -7, 6, 1, 0, 2], [8, -7, 6, 1, 2], [8, -7, 6, 3], [8, -7, 9], [8, 2], [10]])
-
-Test.assert_equals(squish([8, 7], "right"), [[8, 7], [15]])
-
-Test.assert_equals(squish([8], "right"), [[8]])
-
-Test.assert_equals(squish([], "right"), [])
+    return [right, left][d == "left"](lst) if lst else []
