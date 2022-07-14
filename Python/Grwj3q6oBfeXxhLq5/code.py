843bf8f0b1c46284c01fe599c8674ffc00ea6ed1
Alec <amsears60441@gmail.com>
2022-07-13 19:35:45 -0600

    pushing "sexy_triplets" (Grwj3)

--- Python/Grwj3q6oBfeXxhLq5/code.py
+++ Python/Grwj3q6oBfeXxhLq5/code.py
@@ -1,20 +1,32 @@
+
+def is_prime(n):
+    if n == 1:
+        return False
+    return all(n % i != 0 for i in range(2, n))
+
+
+
+
 def sexy_triplets(low, high):
-	
+    def sex(n):
+        
+        triple1 = n
+        triple2 = triple1 + 6
+        triple3 = triple2 + 6
 
-TestsConsoleTest.assert_equals(sexy_triplets(1, 19), [[7, 13, 19]], "Example #1")
+        if is_prime(triple1) and is_prime(triple2) and is_prime(triple3) and not is_prime(triple3 + 6):
+            
+            return [triple1, triple2, triple3]
+    
+    res = []
+    for i in range(low, high-11):
+        if sex(i):
+            res.append(sex(i))
+    return res
 
-Test.assert_equals(sexy_triplets(1, 17), [], "Example #2")
+res = sexy_triplets(64,88)
+print(res)
 
-Test.assert_equals(sexy_triplets(64, 88), [[67, 73, 79]], "Example #3")
 
-Test.assert_equals(sexy_triplets(11, 59), [[17, 23, 29], [31, 37, 43], [47, 53, 59]])
 
-Test.assert_equals(sexy_triplets(17, 29), [[17, 23, 29]])
 
-Test.assert_equals(sexy_triplets(109, 275), [[151, 157, 163], [167, 173, 179], [227, 233, 239], [257, 263, 269]])
-
-Test.assert_equals(sexy_triplets(1000, 1080), [])
-
-Test.assert_equals(sexy_triplets(5842, 6333), [[6067, 6073, 6079], [6257, 6263, 6269], [6317, 6323, 6329]])
-
-Test.assert_equals(sexy_triplets(45000, 45777), [[45427, 45433, 45439], [45491, 45497, 45503], [45751, 45757, 45763]])
