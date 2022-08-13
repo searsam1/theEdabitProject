8b2382a0ebb8058467e81ef7b0cb47fff7ae6c90
Alec <amsears60441@gmail.com>
2022-08-12 23:49:33 -0600

    Create Challenge : iZgvZoGZLkDPmAtNu on 2022-08-12 23:49:33.736343

---
+++ Python/iZgvZoGZLkDPmAtNu/code.py
@@ -0,0 +1,20 @@
+def semiprime(n):
+	
+
+TestsConsoleTest.assert_equals(semiprime(49), "Semiprime")
+
+Test.assert_equals(semiprime(15), "Squarefree Semiprime")
+
+Test.assert_equals(semiprime(19), "Neither")
+
+Test.assert_equals(semiprime(75), "Neither")
+
+Test.assert_equals(semiprime(169), "Semiprime")
+
+Test.assert_equals(semiprime(203), "Squarefree Semiprime")
+
+Test.assert_equals(semiprime(177), "Squarefree Semiprime")
+
+Test.assert_equals(semiprime(125), "Neither")
+
+Test.assert_equals(semiprime(70), "Neither")
--- Python/trPQbGEb9p2yjMAWb/code.py
+++ Python/trPQbGEb9p2yjMAWb/code.py
@@ -1,16 +1,6 @@
-def every_some(test, val, a, b, c, d, e):
-	
+def every_some(test, val, *args):
 
-TestsConsoleTest.assert_equals(every_some(">= 1", "everybody", 1, 1, -1, 1, 1), False, "Example #1")
+    if val == "everybody":
+        return all(eval("{} {}".format(i,test)) for i in args)
+    return any(eval("{} {}".format(i,test)) for i in args)
 
-Test.assert_equals(every_some(">= 1", "somebody", -1, -1, -1, -1, 1), True, "Example #2")
-
-Test.assert_equals(every_some("< 4 / 2", "everybody", 1, 2, 1, 0, -10), False, "Example #3")
-
-Test.assert_equals(every_some("!= 0", "everybody", False, False, False, False, False), False)
-
-Test.assert_equals(every_some("<= 10 * 2", "somebody", 21, 68, 104, 20, 3), True)
-
-Test.assert_equals(every_some("!= 1", "everybody", True, True, True, True, True), False)
-
-Test.assert_equals(every_some("== 9 % 9", "somebody", 9, 1, 81, 218, 33), False)
