5c90ffe7bdb29bff19209aad25e129d1e73062b3
Alec <amsears60441@gmail.com>
2022-08-12 23:33:53 -0600

    Create Challenge : trPQbGEb9p2yjMAWb on 2022-08-12 23:33:53.861683

--- Python/8WvpPQto44PqNLSqJ/code.py
+++ Python/8WvpPQto44PqNLSqJ/code.py
@@ -1,10 +1,13 @@
+
 def pad(message):
-	
+    length = len(message)
+    if length >= 140: return message
+    if length % 2 == 0: message += " "
+    c = 'l'
+    while length < 140:
+        message += c
+        c = ["o", "l"][c == "o"]
+        length = len(message)
+    return message
 
-TestsConsoleTest.assert_equals(pad("Even"), "Even lololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololol")
 
-Test.assert_equals(pad("Odd"), "Oddlolololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololol")
-
-Test.assert_equals(pad("I love the new challenge"), "I love the new challenge lololololololololololololololololololololololololololololololololololololololololololololololololololololololololol")
-
-Test.assert_equals(pad("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dolor purus, finibus eget magna vel, suscipit semper nibh. Quisque posuere."), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dolor purus, finibus eget magna vel, suscipit semper nibh. Quisque posuere.")
