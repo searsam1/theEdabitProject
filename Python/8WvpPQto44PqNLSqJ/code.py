bc66755319612f649ffed1a1740ec16527e47929
Alec <amsears60441@gmail.com>
2022-08-12 23:11:08 -0600

    Create Challenge : 8WvpPQto44PqNLSqJ on 2022-08-12 23:11:08.220453

--- Python/8WvpPQto44PqNLSqJ/code.py
+++ Python/8WvpPQto44PqNLSqJ/code.py
@@ -1,2 +1,10 @@
 def pad(message):
 	
+
+TestsConsoleTest.assert_equals(pad("Even"), "Even lololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololol")
+
+Test.assert_equals(pad("Odd"), "Oddlolololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololol")
+
+Test.assert_equals(pad("I love the new challenge"), "I love the new challenge lololololololololololololololololololololololololololololololololololololololololololololololololololololololololol")
+
+Test.assert_equals(pad("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dolor purus, finibus eget magna vel, suscipit semper nibh. Quisque posuere."), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dolor purus, finibus eget magna vel, suscipit semper nibh. Quisque posuere.")
