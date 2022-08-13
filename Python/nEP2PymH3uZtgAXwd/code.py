6e1822be2b7e1d98f49db64474d5e4ef29aac803
Alec <amsears60441@gmail.com>
2022-08-13 01:59:08 -0600

    Create Challenge : wEDHiAcALvS2KuRBJ on 2022-08-13 01:59:08.220020

--- Python/nEP2PymH3uZtgAXwd/code.py
+++ Python/nEP2PymH3uZtgAXwd/code.py
@@ -1,12 +1,20 @@
+
 def mystery_func(txt):
-	
+    txt = txt.title()
+    print(list(
+        (int(i))
+             for i in txt)
+        )
 
-TestsConsoleTest.assert_equals(mystery_func("hello"), [4, 1, 3, 3, 1])
 
-Test.assert_equals(mystery_func("maybe"), [4, 2, 1, 3, 1])
+mystery_func("hello")
 
-Test.assert_equals(mystery_func("hopeless"), [0, 7, 0, 5, 4, 5, 3, 3])
+# TestsConsoleTest.assert_equals(mystery_func("hello"), [4, 1, 3, 3, 1])
 
-Test.assert_equals(mystery_func("higher"), [2, 3, 1, 2, 5, 0])
+# Test.assert_equals(mystery_func("maybe"), [4, 2, 1, 3, 1])
 
-Test.assert_equals(mystery_func("movement"), [5, 7, 6, 5, 5, 5, 6, 4])
+# Test.assert_equals(mystery_func("hopeless"), [0, 7, 0, 5, 4, 5, 3, 3])
+
+# Test.assert_equals(mystery_func("higher"), [2, 3, 1, 2, 5, 0])
+
+# Test.assert_equals(mystery_func("movement"), [5, 7, 6, 5, 5, 5, 6, 4])
