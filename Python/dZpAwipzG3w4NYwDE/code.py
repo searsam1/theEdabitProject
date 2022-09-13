cd96f5978b08e1b6a944a11bc12adf9a44b77969
Alec <amsears60441@gmail.com>
2022-09-13 03:52:29 -0600

    Create Challenge : tDswMNY7X9h7tyTS4 on 2022-09-13 03:52:29.521511

--- Python/dZpAwipzG3w4NYwDE/code.py
+++ Python/dZpAwipzG3w4NYwDE/code.py
@@ -1,12 +1,26 @@
+
+class Test:
+    def assert_equals(a,b,message=None):
+        assert a == b
+        print("Test Passed.")
+
+
 def highest_pair(cards):
+	c = set(i for i in cards if cards.count(i) >= 2)
 	
+	if "A" in c:
+		return [True, "A"]
+	if "K" in c:
+		return [True, "K"]
+	if "Q" in c:
+		return [True, "Q"]	
+	if "J" in c:
+		return [True, "J"]	
+	return [True, max(c)] if c else False
 
-TestsConsoleTest.assert_equals(highest_pair(['A', 'A', 'K', 'K', '3']), [True, 'A'])
 
+Test.assert_equals(highest_pair(['A', 'A', 'K', 'K', '3']), [True, 'A'])
 Test.assert_equals(highest_pair(['A', 'K', 'Q', 'J', '10']), False)
-
 Test.assert_equals(highest_pair(['A', 'K', 'K', 'K', 'Q']), [True, 'K'])
-
 Test.assert_equals(highest_pair(['A', '3', '3', '4', '4']), [True, '4'])
-
-Test.assert_equals(highest_pair(['A', 'K', 'Q', 'Q', '5']), [True, 'Q'])
+Test.assert_equals(highest_pair(['A', 'K', 'Q', 'Q', '5']), [True, 'Q'])    
---
+++ Python/tDswMNY7X9h7tyTS4/readme.md
@@ -0,0 +1,23 @@
+# Pascal's Triangle
+<br><br>
+## Mubashir Hassan 
+<br><br>
+### """Mubashir was reading about Pascal's triangle on Wikipedia.
+In mathematics, Pascal's triangle is a triangular array of the binomial coefficients that arises in probability theory, combinatorics, and algebra.
+
+Formula for Pascal's triangle is given by:
+
+where n denotes a row of the triangle, and k is the position of a term in the row.
+Create a function which takes a number n and returns n top rows of Pascal's Triangle flattened into a one-dimensional list.
+N/A"""
+<br><br>
+[tDswMNY7X9h7tyTS4](https://edabit.com/challenge/tDswMNY7X9h7tyTS4)
+<br><br>
+```pascals_triangle(1) ➞ [1]
+
+pascals_triangle(2) ➞ [1, 1, 1]
+
+pascals_triangle(4) ➞ [1, 1, 1, 1, 2, 1, 1, 3, 3, 1]
+```
+
+<br><br>
