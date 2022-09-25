3d2d03ed7a4e7f7083664a4119e66507ee1db0f1
Alec <amsears60441@gmail.com>
2022-09-25 00:02:20 -0600

    Create Challenge : 57oGnnqP5sxW2KHi4 on 2022-09-25 00:02:20.529977

---
+++ Python/57oGnnqP5sxW2KHi4/readme.md
@@ -0,0 +1,20 @@
+# Image Manipulation: Invert (Part 1)
+<br><br>
+## Matt
+<br><br>
+### """Images can be described as a 3D list.
+Your task is to create a function that takes a 3D list representation of an image and returns the inverse of that.
+For example, white becomes black, black becomes white, etc.
+A valid RGB value ranges from 0 to 255 (inclusive).
+If the given list contains out of bound values, convert them to the nearest valid one."""
+<br><br>
+[57oGnnqP5sxW2KHi4](https://edabit.com/challenge/57oGnnqP5sxW2KHi4)
+<br><br>
+```# This image has only one white pixel:
+
+[
+  [[255, 255, 255]]
+]
+```
+
+<br><br>
--- Python/LEFxqpwii5DRLMZRJ/code.py
+++ Python/LEFxqpwii5DRLMZRJ/code.py
@@ -1,33 +1,18 @@
-def make_number(n):
-	...
 
-class Test:
-    def assert_equals(a,b):
-        try:
-            assert a == b
-        except AssertionError:
-            print(f"{a}\n > should equal \n\t{b}")
-TestsConsoleTest = Test            
+[[9,10,11,12,13,14,15,16], [18,19,20,21,22]]
 
-TestsConsoleTest.assert_equals(make_number(9), [[2,3,4], [4,5]], "Example #1")
-Test.assert_equals(make_number(8), [], "Example #2")
-Test.assert_equals(make_number(100), [[9,10,11,12,13,14,15,16], [18,19,20,21,22]], "Example #3")
-Test.assert_equals(make_number(5), [[2,3]])
-Test.assert_equals(make_number(18), [[3,4,5,6], [5,6,7]])
-Test.assert_equals(make_number(40), [[6,7,8,9,10]])
-Test.assert_equals(make_number(1), [])
-Test.assert_equals(make_number(3), [[1, 2]])
-Test.assert_equals(make_number(333), [
-  [
-    10, 11, 12, 13, 14, 15, 16,
-    17, 18, 19, 20, 21, 22, 23,
-    24, 25, 26, 27
-  ],
-  [
-    33, 34, 35, 36, 37,
-    38, 39, 40, 41
-  ],
-  [ 53, 54, 55, 56, 57, 58 ],
-  [ 110, 111, 112 ],
-  [ 166, 167 ]
-])
+def get_sum(n):
+
+    blocks = [] 
+    for i in range(1,n):
+        res = [] 
+        while sum(res) < n:
+            res.append(i)
+            i += 1
+        if sum(res) == n:
+            blocks.append(res)
+    print(blocks)
+    return blocks
+
+
+get_sum(100)
