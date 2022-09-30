3a636448863f79123ccbca482640b18fb12040e1
Alec <amsears60441@gmail.com>
2022-09-30 03:10:49 -0600

    Create Challenge : n53MYmznGpPLxM4K2 on 2022-09-30 03:10:49.027248

--- Python/n53MYmznGpPLxM4K2/code.py
+++ Python/n53MYmznGpPLxM4K2/code.py
@@ -1,12 +1,16 @@
 def license(me, agents, others):
-	
+	...
+
+class Test:
+    def assert_equals(a,b):
+        try:
+            assert a == b
+        except AssertionError:
+            print(f"{a}\n > should equal \n\t{b}")
+TestsConsoleTest = Test            
 
 TestsConsoleTest.assert_equals(license("Zebediah", 1, "Bob Jim Becky Pat"), 100)
-
 Test.assert_equals(license("Eric", 2, "Adam Caroline Rebecca Frank"), 40)
-
 Test.assert_equals(license("Aaron", 3, "Jane Max Olivia Sam"), 20)
-
 Test.assert_equals(license("Zebediah", 4, "Bob Jim Becky Pat"), 40)
-
 Test.assert_equals(license("Zebediah", 5, "Bob Jim Becky Pat"), 20)
